3081d9087fadd8f800026843773ea44bd5f21096
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-09-29 13:23:16 -0500

    complete addint item to supabase functionality

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { addListItem } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const form = document.getElementById('add-new');
 
 /* State */
 
 /* Events */
 
 /* Display Functions */
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const item = data.get('item');
+    const quantity = data.get('quantity');
+
+    await addListItem(item, quantity);
+    form.reset();
+});
--- fetch-utils.js
+++ fetch-utils.js
@@ -28,3 +28,8 @@ export async function signOutUser() {
 }
 
 /* Data functions */
+
+export async function addListItem(item, quantity) {
+    const response = await client.from('shopping-list').insert([{ item, quantity }]);
+    return response;
+}
